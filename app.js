8c4c92d10035e5b54e2bf25352df088880570c2b
Nathangois <eunathangois@gmail.com>
2024-11-29 03:47:39 -0300

    nv atual com virtuo

--- app.js
+++ app.js
@@ -31,7 +31,20 @@ const transporter = nodemailer.createTransport({
 
 // Inicializar o Firebase Admin SDK com a conta de serviço
 admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.applicationDefault({
+  //type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\n/g, '\n'), // Substitui os \n pelo caractere de nova linha
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  //universe_domain: process.env.UNIVERSE_DOMAIN,
+  }
+  )
 });
 
 let accessToken = process.env.FIREBASE_ACCESS_TOKEN; // Obtém o token de acesso inicial das variáveis de ambiente
